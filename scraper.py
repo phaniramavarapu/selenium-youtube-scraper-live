e1b5b1f3b43ef622c94110c31885a3a6556c88b6
phaniramavarapu <45114547+phaniramavarapu@users.noreply.github.com>
2022-01-05 10:23:47 +0000

    updated

--- scraper.py
+++ scraper.py
@@ -1,1 +1,50 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+
+def get_driver(): 
+  chrome_options = Options()
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  driver = webdriver.Chrome(options=chrome_options)
+  return driver
+
+def get_videos(driver):
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
+  driver.get(YOUTUBE_TRENDING_URL)
+  videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
+  return videos
+
+
+if __name__ == "__main__":
+  print('Creating the driver')
+  driver = get_driver()
+
+  print('Fetching trending videos')
+  videos = get_videos(driver)
+   
+  print(f'Found {len(videos)} videos')
+
+  print('Parsing the first video')
+
+  #title, url, thumbnail_url, channel, views, uploaded, description
+
+  video = videos[0]
+  title_tag = video.find_element(By.ID, 'video-title')
+  title = title_tag.text
+  url = title_tag.get_attribute('href')
+  thumbnail_tag = video.find_element(By.TAG_NAME, 'img').get_attribute('src')
+  thumbnail_url = thumbnail_tag.get_attribute('src')
+
+  channel_div = video.find_element(By.CLASS_NAME)
+
+  print('Title:', title)
+  print('URL:', url)
+  
